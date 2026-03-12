d117bd05da44e103e7109f6d92c45cebeeae131e
jas-mid <middleton.jas@northeastern.edu>
2026-03-12 14:22:10 +0000

    Basic page structure and Streamlit configuration file setup

---
+++ Pages/contacts_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+#---links to other pages---
+st.page_link("app.py", label="Statistics Comparison Tool", icon="🏠")
+st.page_link("pages/statistics_page.py", label="Statistics Comparison Tool", icon="📊")
+st.page_link("pages/contacts_page.py", label="Contacting Your Council", icon="📱", disabled =True)
--- app.py
+++ app.py
@@ -1,47 +1,15 @@
 import streamlit as st
-import pandas as pd
-#streamlit_folium import folium_static does  maps
- 
-#streamlit tutorial: https://docs.streamlit.io/library/get-started/create-an-app
 
+#---Title and page links---#
 #title
-st.title("hi")
+st.title("Welcome to the East London Council Rating App")
 
-#paragraphs
-st.write("""
-#
-Hello *world!*
-""")
+st.page_link("pages/statistics_page.py", label="Statistics Comparison Tool", icon="📊")
+st.page_link("pages/statistics_page.py", label="Contacting Your Council", icon="📱")
 
-#adding a sidebar with the options
-st.sidebar.header("Sidebar")
-st.sidebar.write("This is the sidebar")
-
-#more sidebar things
-st.sidebar.subheader("Subheader")
-st.sidebar.write("This is the subheader")
+st.divider()
 
 
-#adding dataset to streamlit
-#if wanted in the sidebar, add st.sidebar before the visual and error handling print
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    if uploaded_file.name.endswith('.csv'):
-        df = pd.read_csv(uploaded_file)
-    elif uploaded_file.name.endswith('.xlsx'):
-        df = pd.read_excel(uploaded_file)
-    elif uploaded_file.name.endswith('.json'):
-        df = pd.read_json(uploaded_file)
-    st.success(f"File {uploaded_file.name} uploaded successfully!")
-    st.dataframe(df)
-else:
-    st.error("No file uploaded")
+#---Introduction to the app and what it does---#
+st.header("Goal of the App")
 
-
-#if the data is within the same directory as the app, you can read it directly without uploading
-ready_df = pd.read_csv("my_data.csv")
-st.dataframe(read_csv_dataset)
-
-
-#df = pd.read_csv("my_data.csv")
-#st.line_chart(df)
