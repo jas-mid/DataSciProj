01cc057f8a82145f0844bd8b72f84bc3eb0843a8
jas-mid <middleton.jas@northeastern.edu>
2026-03-12 15:51:43 +0000

    Borough and statistic filters added to statistics page. Minor fixes to data and elsewhere.

--- Pages/statistics_page.py
+++ Pages/statistics_page.py
@@ -10,9 +10,20 @@ col1, col2, col3 = st.columns(3)
 #page links
 col1.page_link("app.py", label="Home", icon="🏠")
 col2.page_link("pages/statistics_page.py", label="Statistics Comparison Tool", icon="📊",disabled=True)
-col3.page_link("pages/contacts_page.py", label="Contacting Your Council", icon="📱")
+col3.page_link("pages/contacts_page.py", label="Contacting Your Borough", icon="📱")
 st.divider()
 
 #---Title and introduction to the page---
 st.header("Statistics Comparison Tool")
-st.markdown("This page allows you to compare the statistics of different councils in East London")
+st.markdown("This page allows you to compare the statistics of different Boroughs in East London")
+
+#---Load and clean the data---
+data = load_and_clean_data("EDSP_council_performance_data.csv")
+
+#---Select Boroughs to compare---
+boroughs = data[data["Borough"] != "Greater London Average"]["Borough"].unique()
+selected_boroughs = st.sidebar.multiselect("Select Boroughs to Compare", options=boroughs, default=boroughs[:0])
+
+#---Select which metrics you want to see---
+metrics = data.columns[1:]  #as the first column is 'Borough'
+selected_metrics = st.multiselect("Select Metrics to Compare", options=metrics, default=metrics[:0])
