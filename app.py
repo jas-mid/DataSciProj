e24b2fcfee815a96bc32c678110cdcd5b3c4cbf9
jas-mid <middleton.jas@northeastern.edu>
2026-03-13 09:19:33 +0000

    Theme ideas tweaked. Landing page updated to have more information.

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 #---Title and page links---#
 #title
-st.title("Welcome to the East London Council Rating App")
+st.markdown("<h1 style='text-align: center; color: white;'>East London Council Comparison App</h1>", unsafe_allow_html=True)
+st.markdown("Pages:")
 
 st.page_link("pages/statistics_page.py", label="Statistics Comparison Tool", icon="📊")
 st.page_link("pages/statistics_page.py", label="Contacting Your Council", icon="📱")
@@ -13,6 +14,7 @@ st.divider()
 #---Introduction to the app and what it does---
 
 #using markdown to make the text bigger and centered
-st.markdown("<h1 style='text-align: center; color: white;'>Goal of the App</h1>", unsafe_allow_html=True)
-
+st.title("Goal Of The App")
+st.markdown("Councils in the UK are often felt to not represent their constituents very well. In a time where only 34% of people trust their local government, it is more important than ever to bring clarity.")
+st.page_link("https://www.ons.gov.uk/peoplepopulationandcommunity/wellbeing/bulletins/trustingovernmentuk/2023", label="ONS Survey on Trust in Government", icon="🔗")
 
