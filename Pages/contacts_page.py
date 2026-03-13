01e9aba8b144adf2fd5309cea91480d7ace3a393
jas-mid <middleton.jas@northeastern.edu>
2026-03-13 09:19:33 +0000

    Contacts page coded and links added. Minor fixes to statistics page.

--- Pages/contacts_page.py
+++ Pages/contacts_page.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+from data_handling import load_and_clean_data
 
 #---links to other pages---
 
@@ -9,3 +11,55 @@ col1.page_link("app.py", label="Home", icon="🏠")
 col2.page_link("pages/statistics_page.py", label="Statistics Comparison Tool", icon="📊")
 col3.page_link("pages/contacts_page.py", label="Contacting Your Council", icon="📱", disabled =True)
 st.divider()
+
+#---Load and clean the data---
+data = load_and_clean_data("EDSP_council_performance_data.csv")
+#gaining boroughs
+boroughs = sorted(
+    [b for b in data["Borough"].unique() if b != "Greater London Average"]
+)
+
+#---Laying out varaibles for the page---
+selected_borough = "placeholder"
+
+#---Title and introduction to the page---
+st.header("Getting Your Voice Heard")
+
+#---Selecting your council in the sidebar---
+st.sidebar.subheader("Select Your Council")
+for borough in boroughs:
+    if st.sidebar.button(borough):
+        selected_borough = borough
+
+#---Displaying the contact information for the selected council---
+
+#function to display the contact information for the selected council
+def display_contact_info(selected_borough):
+
+    #displays the council name
+    st.subheader(selected_borough + " Council")
+
+    #initialising link to the website and determining what it will be based on the selected borough
+    link=""
+    if selected_borough == "Barking & Dagenham":
+        link = "https://www.lbbd.gov.uk/contact-us/"
+    elif selected_borough == "Hackney":
+        link = "https://hackney.gov.uk/contact-us"
+    elif selected_borough == "Havering":
+        link = "https://www.havering.gov.uk/contactus"
+    elif selected_borough == "Newham":
+        link = "https://www.newham.gov.uk/contact-information"
+    elif selected_borough == "Redbridge":
+        link = "https://www.redbridge.gov.uk/contact-us/"
+    elif selected_borough == "Tower Hamlets":
+        link = "https://www.towerhamlets.gov.uk/content_pages/contact_us/contact-us.aspx"
+    elif selected_borough == "Waltham Forest":
+        link = "https://www.walthamforest.gov.uk/contact-us"
+    
+
+    st.page_link(link, label="Contact " + selected_borough + " Council", icon="📱")
+
+if selected_borough == "placeholder":
+    st.warning("Select your council to see how you can get your opinion to them")#
+else:
+    display_contact_info(selected_borough)
--- Pages/statistics_page.py
+++ Pages/statistics_page.py
@@ -15,15 +15,46 @@ st.divider()
 
 #---Title and introduction to the page---
 st.header("Statistics Comparison Tool")
-st.markdown("This page allows you to compare the statistics of different Boroughs in East London")
+st.markdown("This page allows you to compare the statistics of different measures that Boroughs in East London have direct control over")
 
 #---Load and clean the data---
 data = load_and_clean_data("EDSP_council_performance_data.csv")
 
 #---Select Boroughs to compare---
-boroughs = data[data["Borough"] != "Greater London Average"]["Borough"].unique()
-selected_boroughs = st.sidebar.multiselect("Select Boroughs to Compare", options=boroughs, default=boroughs[:0])
+st.sidebar.subheader("Select Boroughs to Compare:")
+boroughs = sorted(
+    [b for b in data["Borough"].unique() if b != "Greater London Average"]
+)
+selected_boroughs = ["Greater London Average"]  # Always include Greater London Average for comparison
+for borough in boroughs:
+    if st.sidebar.checkbox(borough):
+        selected_boroughs.append(borough)
+
+
 
 #---Select which metrics you want to see---
 metrics = data.columns[1:]  #as the first column is 'Borough'
-selected_metrics = st.multiselect("Select Metrics to Compare", options=metrics, default=metrics[:0])
+selected_metrics = st.selectbox("Select Metrics to Compare", metrics)
+
+#---Filter the data based on user selections---
+plot_data = data[data["Borough"].isin(selected_boroughs)]
+
+if selected_metrics:
+
+    metric = selected_metrics
+
+    fig = px.bar(
+        plot_data,
+        x="Borough",
+        y=metric,
+        color="Borough",
+        title=f"{metric} Comparison"
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+else:
+    st.warning("Select what data you want to display.")
+
+if selected_metrics == "Debt in GBP":
+    st.warning("There is no wide data for the Greater London Average for this metric, so it is not included in the graph.")
