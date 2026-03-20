d10974ca0e6e095dc606e65c61b1e306f403e4a4
jas-mid <middleton.jas@northeastern.edu>
2026-03-20 09:10:32 +0000

    Created test_sample file and outlined tests

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+import pytest
+
+class TestDataHandoling:
+    from data_handling import load_and_clean_data, convert_percentage_to_numeric, population_to_numeric
+
+
+class TestcontactsPage:
+    from Pages.contacts_page import display_contact_info, display_voting_info
+
+class TestStatisticsPage:
+    from Pages.statistics_page import create_comparison_graph, filter_data_by_metric
+
